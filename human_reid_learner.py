5801e1b18cb1780a2d469b7f0e27117a98de1cf6
tony joseph <tony.joseph@uoit.net>
2018-06-03 00:24:19 -0400

    Updated

--- human_reid_learner.py
+++ human_reid_learner.py
@@ -32,7 +32,7 @@ class reid_Learner(object):
         identity_loss_l = nn.NLLLoss(reduce=True)
         identity_loss_r = nn.NLLLoss(reduce=True)
         pairwise_distance = nn.PairwiseDistance(p=2)
-        HingeEmbeddLoss   = nn.HingeEmbeddingLoss(margin=opt.margin, reduce=True)
+        HingeEmbeddLoss = nn.HingeEmbeddingLoss(margin=opt.margin, reduce=True)
 
         # Build graph
         if opt.use_opt_flow == True:
@@ -43,10 +43,13 @@ class reid_Learner(object):
         net.train()
 
         # GPU
-        net = net.cuda()
+        if opt.use_gpu:
+            net = net.cuda()
+        else:
+            print('GPU unavailable!')
 
         # Optimizer
-        optimizer = optim.SGD(net.parameters(), lr=opt.l_rate, momentum=0.9)
+        optimizer = optim.SGD(net.parameters(), lr=opt.l_rate,  weight_decay=opt.l2, momentum=0.9)
 
         # Check if training has to be continued
         if opt.continue_train:
@@ -56,11 +59,10 @@ class reid_Learner(object):
                 load_model = os.path.join(opt.checkpoint_dir, opt.init_checkpoint_file)
             print("Resume training from previous checkpoint: %s" % opt.init_checkpoint_file)
             net.load_state_dict(torch.load(load_model))
-        
-        # Zero-state
-        hidden_state=None
+
         # Begin Training
         for eph in range(opt.start_step, opt.max_steps):
+            interm_loss = 0.0
             for i in range(opt.nPersons*2):
                 if i%2 == 0:
                     output_feed = train_dataLoader.gen_data_batch(opt.batch_size, int(i/2), 1, data_augmentation=True)
@@ -70,28 +72,33 @@ class reid_Learner(object):
                     l_batch, r_batch, p_n_batch, l_idx_batch, r_idx_batch = output_feed
 
                 # Input data
-                print(l_idx_batch)
-                l_batch     = torch.tensor(l_batch,  dtype=torch.float).cuda()
-                r_batch     = torch.tensor(r_batch,  dtype=torch.float).cuda()
-                #p_n_batch   = torch.tensor(p_n_batch, dtype=torch.long).cuda()
-                l_idx_batch = torch.tensor(l_idx_batch, dtype=torch.long).cuda()
-                r_idx_batch = torch.tensor(r_idx_batch, dtype=torch.long).cuda()
+                if opt.use_gpu:
+                    # Convert to GPU
+                    l_batch     = torch.tensor(l_batch,     dtype=torch.float).cuda()
+                    r_batch     = torch.tensor(r_batch,     dtype=torch.float).cuda()
+                    l_idx_batch = torch.tensor(l_idx_batch, dtype=torch.long).cuda()
+                    r_idx_batch = torch.tensor(r_idx_batch, dtype=torch.long).cuda()
+                    p_n_batch   = torch.tensor(p_n_batch,   dtype=torch.long).cuda()
+                else:
+                    l_batch     = torch.tensor(l_batch,     dtype=torch.float)
+                    r_batch     = torch.tensor(r_batch,     dtype=torch.float)
+                    l_idx_batch = torch.tensor(l_idx_batch, dtype=torch.long)
+                    r_idx_batch = torch.tensor(r_idx_batch, dtype=torch.long)
+                    p_n_batch   = torch.tensor(p_n_batch,   dtype=torch.long)
 
+                # Convert to Variable
                 l_batch = Variable(l_batch)
                 r_batch = Variable(r_batch)
-                #p_n_batch   = Variable(p_n_batch)
+                p_n_batch   = Variable(p_n_batch)
                 l_idx_batch = Variable(l_idx_batch)
                 r_idx_batch = Variable(r_idx_batch)
 
                 # Forward pass
-                #l_output, r_output, l_id, r_id, hidden_state = net(l_batch, r_batch, steps=opt.sequence_length, hidden=hidden_state)
-                l_id = net(l_batch, steps=opt.sequence_length)
+                l_output, r_output, l_id, r_id, hidden = net(l_batch, r_batch, opt.sequence_length, hidden=None)
 
-#                # Compute loss
-#                l2_dist = pairwise_distance(l_output, r_output)
-#                loss = HingeEmbeddLoss(l2_dist, p_n_batch) + identity_loss_l(l_id, l_idx_batch) + identity_loss_r(r_id, r_idx_batch)
-                 #loss = identity_loss_l(l_id, l_idx_batch) + identity_loss_r(r_id, r_idx_batch)
-                loss = identity_loss_l(l_id, l_idx_batch)
+                # Compute loss
+                l2_dist = pairwise_distance(l_output, r_output)
+                loss = HingeEmbeddLoss(l2_dist, p_n_batch) + identity_loss_l(l_id, l_idx_batch) + identity_loss_r(r_id, r_idx_batch)
 
                 # Run Optim
                 loss.backward()
@@ -102,12 +109,15 @@ class reid_Learner(object):
                 # Update the weights
                 optimizer.step()
 
-                if i % opt.summary_freq == 0:
-                    interm_loss = loss.clone().cpu().data.numpy()
-                    writer.add_scalar('loss', interm_loss, i)
-                    for name, param in net.named_parameters():
-                        writer.add_histogram(name, param.clone().cpu().data.numpy(), i)
-                    print('Loss: ', interm_loss, ' at Epoch: ', str(eph + 1), ' iteration: ', i)
+                # Accumlate loss
+                interm_loss += loss.clone().cpu().data.numpy()
+
+            if eph % opt.summary_freq == 0:
+                interm_loss = interm_loss/(opt.nPersons*2)
+                writer.add_scalar('loss', interm_loss, eph)
+                for name, param in net.named_parameters():
+                    writer.add_histogram(name, param.clone().cpu().data.numpy(), eph)
+                print('Loss: ', np.round(interm_loss, 3), ' at Epoch: ', str(eph + 1))
 
             if eph % opt.save_latest_freq == 0:
                 model_name = 'hnRiD_' + str(eph) + '.ckpt'
@@ -121,5 +131,5 @@ class reid_Learner(object):
         print("Training Complete and latest checkpoint saved!")
 
         # Export scalar data to JSON for external processing
-        writer.export_scalars_to_json("./logs/" + str(opt.exp_name) + ".json")
+        writer.export_scalars_to_json(str(opt.exp_name) + ".json")
         writer.close()
