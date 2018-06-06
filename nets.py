aaea3ba7b8a2dfdbbfc63082385ccbe710aceb9e
vclab <josephtony1993@yahoo.com>
2018-06-06 19:33:20 -0400

    Updated

--- nets.py
+++ nets.py
@@ -15,19 +15,19 @@ class Net(nn.Module):
         stepSize = 2
         # Conv
         self.conv1 = nn.Conv2d(nChannel, 16, filtsize, stride=1)
-        self.tanh1 = nn.Tanh()
+        self.tanh1 = nn.ReLU()
         self.pool1 = nn.MaxPool2d(poolsize, stride=stepSize)
 
         self.conv2 = nn.Conv2d(16, 32, filtsize, stride=1)
-        self.tanh2 = nn.Tanh()
+        self.tanh2 = nn.ReLU()
         self.pool2 = nn.MaxPool2d(poolsize, stride=stepSize)
 
         self.conv3 = nn.Conv2d(32, 32, filtsize, stride=1)
-        self.tanh3 = nn.Tanh()
+        self.tanh3 = nn.ReLU()
 
         # FC
         self.fc    = nn.Linear(32*7*3, embeddingSize)
-        self.tanh4 = nn.Tanh()
+        self.tanh4 = nn.ReLU()
         self.drop  = nn.Dropout(p=drop_prob)
 
         # Classification Layer
@@ -35,14 +35,20 @@ class Net(nn.Module):
         self.log_softmax     = nn.LogSoftmax(dim=1)
 
         # RNN
-        self.rnn   = nn.RNN(input_size=embeddingSize,
+        self.rnn   = nn.LSTM(input_size=embeddingSize,
                             hidden_size=embeddingSize,
                             num_layers=1,
-                            nonlinearity='tanh',
-                            batch_first=True,
-                            dropout=drop_prob)
+                            batch_first=True)
 
-    def forward_pass(self, x, hidden):
+        # Initialize weights:
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+    def conv_pass(self, x):
        # Convolutional Layer --1
         x = self.conv1(x)
         x = self.tanh1(x)
@@ -59,38 +65,40 @@ class Net(nn.Module):
         x = self.fc(x)
         x = self.tanh4(x)
         x = self.drop(x)
-        # RNN
+        # Expand dims
         x = x.unsqueeze(1) # (B, 1, embeddingSize)
-        x, hidden = self.rnn(x, hidden)
 
-        return x, hidden
+        return x
 
-    def forward(self, x1, x2, steps, hidden=None):
-        left_outputs = []
-        right_outputs = []
-        # Number of steps
+    def forward_pass(self, x, hidden=None):
+        outputs = []
+        # CNN
         for t in range(steps):
             # Forward pass for cam-1
-            output1, hidden = self.forward_pass(x1[:, t, :, :, :], hidden)
-            # Forward pass for cam-2
-            output2, hidden = self.forward_pass(x2[:, t, :, :, :], hidden)
+            output = self.forward_pass(x[:, t, :, :, :])
             # Append features
-            left_outputs.append(output1)
-            right_outputs.append(output2)
-
+            outputs.append(output)
+        # RNN
+        outputs  = torch.cat(outputs, dim=1) # N * [B, 1, 128] --> [B, N, 128]
+        outputs, hidden1 = self.rnn(outputs, hidden)
         # Temporal Pooling
-        left_outputs  = torch.cat(left_outputs,   dim=1) # N * [B, 1, 128] --> [B, N, 128]
-        right_outputs = torch.cat(right_outputs,  dim=1) # N * [B, 1, 128] --> [B, N, 128]
-        left_output   = torch.mean(left_outputs,  dim=1) # [B, 1, 128]
-        right_output  = torch.mean(right_outputs, dim=1) # [B, 1, 128]
+        output   = torch.mean(outputs,  dim=1) # [B, 1, 128]
+        # Classifier
+        output  = output.view(-1, 128)  # (B, 128)
+        id = self.classifierLayer(output)
+        id = self.log_softmax(id)
 
-        left_output  = left_output.view(-1, 128)  # (B, 128)
-        right_output = right_output.view(-1, 128) # (B, 128)
+        return output, id
 
-        l_id = self.classifierLayer(left_output)
-        r_id = self.classifierLayer(right_output)
+    def forward(self, x1, x2, steps, hidden=None):
+        left_output = []
+        right_output = []
 
-        l_id = self.log_softmax(l_id)
-        r_id = self.log_softmax(r_id)
+        #--------------------------- Left Input --------------------------------
+        left_output, l_id  = forward_pass(x1)
 
-        return left_output, right_output, l_id, r_id, hidden
+        #--------------------------- Right Input -------------------------------
+        right_output, r_id = forward_pass(x2)
+
+
+        return left_output, right_output, l_id, r_id
