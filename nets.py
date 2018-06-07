76f99acf0dfde70bfd9b3e31218c69de32e22b76
vclab <josephtony1993@yahoo.com>
2018-06-06 23:17:00 -0400

    Updated

--- nets.py
+++ nets.py
@@ -14,83 +14,76 @@ class Net(nn.Module):
         filtsize = 5
         poolsize = 2
         stepSize = 2
+        # Batch size:
+        self.batch_size = batch_size
+        # Hidden Size
+        self.embeddingSize = embeddingSize
+
         # Conv
-        self.conv1 = nn.Conv2d(nChannel, 16, filtsize, stride=1)
-        self.tanh1 = nn.ReLU()
-        self.pool1 = nn.MaxPool2d(poolsize, stride=stepSize)
+        self.cnn = nn.Sequential(
+                   # Layer 1
+                   nn.Conv2d(nChannel, 16, filtsize, stride=1),
+                   nn.Tanh(),
+                   nn.MaxPool2d(poolsize, stride=stepSize),
 
-        self.conv2 = nn.Conv2d(16, 32, filtsize, stride=1)
-        self.tanh2 = nn.ReLU()
-        self.pool2 = nn.MaxPool2d(poolsize, stride=stepSize)
+                   # Layer 2
+                   nn.Conv2d(16, 32, filtsize, stride=1),
+                   nn.Tanh(),
+                   nn.MaxPool2d(poolsize, stride=stepSize),
 
-        self.conv3 = nn.Conv2d(32, 32, filtsize, stride=1)
-        self.tanh3 = nn.ReLU()
+                   # Layer 3
+                   nn.Conv2d(32, 32, filtsize, stride=1),
+                   nn.Tanh(),
 
-        # FC
-        self.fc    = nn.Linear(32*7*3, embeddingSize)
-        self.tanh4 = nn.ReLU()
-        self.drop  = nn.Dropout(p=drop_prob)
+                   # FC
+                   nn.Linear(32*7*3, embeddingSize),
+                   nn.Tanh(),
+                   nn.Dropout(p=drop_prob),
+                   )
 
         # Classification Layer
-        self.classifierLayer = nn.Linear(embeddingSize, nPerson)
-        self.log_softmax     = nn.LogSoftmax(dim=1)
+        self.classify = nn.Sequential(
+                        nn.Linear(embeddingSize, nPerson)
+                        nn.LogSoftmax(dim=1))
 
         # RNN
         self.rnn   = nn.RNN(input_size=embeddingSize,
                             hidden_size=embeddingSize,
                             num_layers=1,
+                            nonlinearity='tanh',
                             batch_first=False,
                             dropout=drop_prob)
 
-        # Batch size:
-        self.batch_size = batch_size
-        self.embeddingSize = embeddingSize
-
         # Initialize weights:
         for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='tanh')
             elif isinstance(m, nn.BatchNorm2d):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
 
     def net_step(self, x, hidden):
-       # Convolutional Layer --1
-        x = self.conv1(x)
-        x = self.tanh1(x)
-        x = self.pool1(x)
-       # Convolutional Layer --2
-        x = self.conv2(x)
-        x = self.tanh2(x)
-        x = self.pool2(x)
-       # Convolutional Layer --3
-        x = self.conv3(x)
-        x = self.tanh3(x)
-        # FC
-        x = x.view(-1, 32*7*3) # (B, 672)
-        x = self.fc(x)
-        x = self.tanh4(x)
-        x = self.drop(x)
+        x = self.cnn(x)
         # Expand dims- (1, B, 128)
         x = x.unsqueeze(0)
         # RNN
-        output, hidden = self.rnn(input, hidden)
+        x, hidden = self.rnn(x, hidden)
 
-        return output, hidden
+        return x, hidden
 
     def forward_pass(self, x, steps, hidden=None):
-        outputs = Variable(torch.zeros(steps, self.batch_size, self.embeddingSize), dtype=torch.float).cuda()
-        # CNN
+        outputs = torch.tensor(torch.zeros(steps, self.batch_size, self.embeddingSize), requires_grad=True)
+        # CNN--> RNN
         for t in range(steps):
-            # Forward pass for cam-1
-            output, hidden = self.net_step(x[:, t, :, :, :], hidden)
+            # Forward pass
+            output, hidden = self.net_step(x[:, t, :, :, :], hidden) # 1, B, 128
+            output = output.squeeze(0)
             # Append features
             outputs[t] = output
         # Temporal Pooling
-        output = torch.mean(outputs,  dim=0) # [B, 128]
+        output = nn.Parameter(torch.mean(outputs, dim=0), requires_grad=True)
         # Classifier
-        id = self.classifierLayer(output)
-        id = self.log_softmax(id)
+        id = self.classify(output)
 
         return output, id
 
